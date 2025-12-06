325e2b8e076f70f9dc13ed092074863798f851c0
JJ Tsai <jia.jiuan.tsai@gmail.com>
2025-12-06 11:41:42 +0100

    Set up NetworkHelper

---
+++ BookBeat/Helpers/Networking.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+protocol Networking {
+    func request<T: Decodable>(urlString: String, as type: T.Type) async throws -> T
+}
+
+struct NetworkHelper: Networking {
+    private let session: URLSession
+    private let decoder: JSONDecoder
+
+    init(session: URLSession = .shared, decoder: JSONDecoder = .init()) {
+        self.session = session
+        self.decoder = decoder
+    }
+
+
+     func request<T: Decodable>(urlString: String, as type: T.Type) async throws -> T {
+            guard let url = URL(string: urlString) else {
+                throw NetworkError.invalidURL(urlString)
+            }
+
+            let (data, response) = try await session.data(from: url)
+
+            guard let httpResponse = response as? HTTPURLResponse else {
+                throw NetworkError.invalidResponse
+            }
+
+            guard (200...299).contains(httpResponse.statusCode) else {
+                throw NetworkError.httpError(statusCode: httpResponse.statusCode)
+            }
+
+            do {
+                return try decoder.decode(type.self, from: data)
+            } catch {
+                throw NetworkError.decoding(error)
+            }
+        }
+}
+
+private enum NetworkError: Error {
+    case invalidURL(String)
+    case invalidResponse
+    case httpError(statusCode: Int)
+    case decoding(Error)
+}
