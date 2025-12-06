314e15dd99f28bee6c9c6ccabbb19691b1a2bc05
JJ Tsai <jia.jiuan.tsai@gmail.com>
2025-12-06 10:16:20 +0100

    Initial Commit

---
+++ BookBeat/BookBeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookBeatApp.swift
+//  BookBeat
+//
+//  Created by Jia-Jiuan Tsai on 2025-12-06.
+//
+
+import SwiftUI
+
+@main
+struct BookBeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookBeatTests/BookBeatTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookBeatTests.swift
+//  BookBeatTests
+//
+//  Created by Jia-Jiuan Tsai on 2025-12-06.
+//
+
+import Testing
+@testable import BookBeat
+
+struct BookBeatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
